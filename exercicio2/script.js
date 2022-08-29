046dd62dc5feab86645c70e27ce1ceaed8fecedd
MailsonAndradeCardoso <mailsoncardoso1988@gmail.com>
2022-08-29 16:00:22 -0300

    Exercicios completos

---
+++ exercicio2/script.js
@@ -0,0 +1,12 @@
+//#Exercicio 2
+
+const primPergunta =("Você torce para o Grêmio?")
+const rsp1 = prompt(primPergunta)
+const segPergunta =("Você torce para o Inter?")
+const rsp2 = prompt(segPergunta)
+const terPergunta =("Você torce para o Juventude?")
+const rsp3 = prompt (terPergunta)
+    
+    console.log(primPergunta, rsp1)
+    console.log(segPergunta, rsp2)
+    console.log(terPergunta, rsp3)
---
+++ exercicio3/script.js
@@ -0,0 +1,14 @@
+//Exercico 3
+
+let a = 10
+let b = 25
+
+let antigoValor = a
+
+//Invertendo so valores de A e B
+
+a = b
+b = antigoValor
+
+console.log("o novo valor de a é", a)
+console.log("o novo valor de b é", b)
